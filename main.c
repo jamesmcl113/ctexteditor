775eeb8fa29262c4acb3970d36962944814f6ff6
James Mclaughlin <sc19jm2@leeds.ac.uk>
2021-05-26 17:47:36 +0100

    merged to single file, cleaned up

--- main.c
+++ main.c
@@ -4,18 +4,37 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#include "editor.h"
-
 #define CTRL(c) ((c) & 0x1f)
-
-Editor e;
 
 /*
 TODO:
-    Add error function
-    Rename File struct 
-
+    Add file saving, output etc..
 */
+
+/** structs **/
+typedef struct {
+    int length;
+    char* data;
+} erow;
+
+typedef struct {
+    int len;
+    erow* rows;
+} FileData;
+
+typedef struct {
+    int x;
+    int y;
+    int nrows;
+    int ncols;
+    int of;
+    char fl[128];
+    char statusmsg[64];
+    FileData* fd;
+} Editor;
+
+Editor e;
+int editFlag = 0;
 
 void die(const char *s)
 {
@@ -24,6 +43,7 @@ void die(const char *s)
     exit(1);
 }
 
+/** buffer **/
 typedef struct {
     char* data;
     int len;
@@ -46,6 +66,7 @@ void freeBuf(buf* b)
     free(b->data);
 }
 
+/** output **/
 void drawRows(buf* b)
 {
     move(0, 0);
@@ -75,31 +96,112 @@ void drawRows(buf* b)
     }
 }
 
-void moveCursor(int c);
-
-void executeCommand(int c)
+void drawStatusLine()
 {
-    char x = c;
-
-    strncat(e.commandBuf, &x, 1);
-
-    int executed = 0;
-    if(!strcmp(e.commandBuf, "G")) moveCursor('.');
-    else if(!strcmp(e.commandBuf, "gg")) moveCursor('/');
-    else executed = 1;
-
-    if(strlen(e.commandBuf) == 2 || !executed) e.commandBuf[0] = '\0';
+    attrset(A_BOLD);
+    move(e.nrows - 1, 0);
+    clrtoeol();
+    mvprintw(e.nrows - 1, 0, "> [%s], %d lines", e.fl, e.fd->len);
+    mvprintw(e.nrows - 1, e.ncols / 2, "%d , %d", e.y + e.of + 1, e.x + 1);
+    //mvprintw(e.nrows - 1, e.ncols / 2, "|");
+    mvprintw(e.nrows - 1, e.ncols - 30, "%s", e.statusmsg);
+    attrset(A_NORMAL);
 }
 
-void handleKeypress(int c)
+void refreshScreen(FileData* fd)
 {
-    if(c == KEY_DOWN || c == KEY_UP || c == KEY_RIGHT || c == KEY_LEFT) moveCursor(c);
-    //else executeCommand(c);
-    else if(c == 127) editorDelChar(&e.fd->rows[e.y + e.of], e.x);
-    else if(c > 31 && e.fd->len > 0) editorInsertChar(&e.fd->rows[e.y + e.of], e.x, c);
-    mvprintw(0, 0, "%i", c);
+    buf b = BUF_INIT;
+
+    // get row data and output to screen
+    drawRows(&b);
+    mvprintw(0, 0, b.data);
+
+    drawStatusLine();
+    move(e.y, e.x);
 }
 
+/** file i/o **/
+FileData* openFile(char* fn)
+{
+    if(!strcmp(fn, "NONE"))
+    {
+        FileData* fd = malloc(sizeof *fd);
+        fd->len = 0;
+        fd->rows = NULL;
+        return fd;
+    }
+
+    FILE* fp = fopen(fn, "rb");
+    if(fp == NULL) die("fopen");
+
+    FileData* fd = malloc(sizeof *fd);
+    if(fd == NULL) die("malloc filedata");
+    fd->len = 0;
+    fd->rows = NULL;
+
+    char* line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    while((linelen = getline(&line, &linecap, fp)) != -1) 
+    {
+        while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) linelen--;
+
+        erow* tmp = realloc(fd->rows, (fd->len + 1) * sizeof(erow));
+        if(tmp == NULL) die("realloc rows");
+        fd->rows = tmp;
+
+        fd->rows[fd->len].data = malloc(linelen + 1);
+        memcpy(fd->rows[fd->len].data, line, linelen);
+        fd->rows[fd->len].data[linelen] = '\0';
+        fd->rows[fd->len].length = linelen;
+        fd->len++;
+    }
+
+    free(line);
+    fclose(fp);
+
+    return fd;
+}
+
+/** editor operations **/
+void editorInit(char* fl)
+{
+    e.x = 0;
+    e.y = 0;
+    e.nrows = getmaxy(stdscr);
+    e.ncols = getmaxx(stdscr);
+    e.of = 0;
+    e.statusmsg[0] = '\0';
+    strcpy(e.fl, fl);
+    e.fd = openFile(fl);
+}
+
+void editorInsertChar(erow* row, int at, char c)
+{
+    if(at < 0 || at > row->length) at = row->length;
+    row->data = realloc(row->data, row->length + 2);
+    memmove(&row->data[at + 1], &row->data[at], row->length - at + 1);
+    row->length++;
+    row->data[at] = c;
+    row->data[row->length] = '\0';
+}   
+
+void editorDelChar(erow* row, int at)
+{
+    if(at < 0 || at >= row->length) return;
+    memmove(&row->data[at], &row->data[at + 1], row->length - at);
+    row->length--;
+}
+
+void editorSetStatusMessage(const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(e.statusmsg, sizeof(e.statusmsg), fmt, ap);
+    va_end(ap);
+}
+
+/** input **/
 void moveCursor(int c)
 {
     int ncols, nrows, y, x;
@@ -148,11 +250,11 @@ void moveCursor(int c)
             break;
         case '.':
             e.of = maxOffset;
-            y = nrows - 2;
+            e.y = nrows - 2;
             break;
         case '/':
             e.of = 0;
-            y = 0;
+            e.y = 0;
             break;
         default:
             return;
@@ -165,38 +267,17 @@ void moveCursor(int c)
     move(e.y, e.x);
 }
 
-void setStatusMessage(const char* fmt, ...)
+void handleKeypress(int c)
 {
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(e.statusmsg, sizeof(e.statusmsg), fmt, ap);
-    va_end(ap);
+    if(c == KEY_DOWN || c == KEY_UP || c == KEY_RIGHT || c == KEY_LEFT) moveCursor(c);
+    else if(c == CTRL('j')) moveCursor('.');
+    else if(c == CTRL('k')) moveCursor('/');
+    else if(c == CTRL('e')) {editFlag = !editFlag; editorSetStatusMessage("EDIT MODE %s", editFlag ? "ON" : "OFF"); }
+    else if(c == 127 && editFlag) editorDelChar(&e.fd->rows[e.y + e.of], e.x);
+    else if(c > 31 && e.fd->len > 0 && editFlag) editorInsertChar(&e.fd->rows[e.y + e.of], e.x, c);
 }
 
-void drawStatusLine()
-{
-    attrset(A_BOLD);
-    move(e.nrows - 1, 0);
-    clrtoeol();
-    mvprintw(e.nrows - 1, 0, "> %s", e.fl);
-    mvprintw(e.nrows - 1, e.ncols / 4, "%d , %d / %d", e.y + e.of + 1, e.x + 1, e.fd->len);
-    mvprintw(e.nrows - 1, e.ncols / 2, "|");
-    mvprintw(e.nrows - 1, (e.ncols / 2) + 4, "%s", e.statusmsg);
-    attrset(A_NORMAL);
-}
-
-void refreshScreen(FileData* fd)
-{
-    buf b = BUF_INIT;
-
-    // get row data and output to screen
-    drawRows(&b);
-    mvprintw(0, 0, b.data);
-
-    drawStatusLine();
-    move(e.y, e.x);
-}
-
+/** init **/
 int main(int argc, char* argv[])
 {
     initscr();
@@ -207,10 +288,10 @@ int main(int argc, char* argv[])
     int nrows = getmaxy(stdscr);
     int ncols = getmaxx(stdscr);
 
-    if(argc == 2) editorInit(&e, nrows, ncols, argv[1]);
-    else editorInit(&e, nrows, ncols, "NONE");
+    if(argc == 2) editorInit(argv[1]);
+    else editorInit("NONE");
 
-    setStatusMessage("HELP: CTRL-Q to quit");
+    editorSetStatusMessage("HELP: CTRL-Q to quit");
 
     int c;
     refreshScreen(e.fd);
